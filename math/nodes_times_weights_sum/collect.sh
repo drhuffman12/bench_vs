af311a35b0b06eb04bbe5a9a5868ed5e1186cc20
Daniel Huffman <drhuffman12@yahoo.com>
2016-07-29 02:23:10 -0400

    nodes_times_weights_sum: clean up collect.sh and show app versions in 'stats_times.log'

--- math/nodes_times_weights_sum/collect.sh
+++ math/nodes_times_weights_sum/collect.sh
@@ -1,5 +1,9 @@
 # bash collect.sh > stats_times.log 2>&1
 
+QTY=1000
+
+## Make RVM and CRENV happy:
+
 # exec $SHELL -l
 PATH="$HOME/.crenv/bin:$PATH"
 eval "$(crenv init -)"
@@ -21,16 +25,16 @@ else
 
 fi
 
+## System Info:
+
 echo
 lscpu
 
 echo
 free -m
 
-echo
-echo $(pwd)
+## Stats for previous/develop versions/mode:
 
-QTY=1000
 echo
 LOG_FILE='stats_previous'
 echo $LOG_FILE
@@ -41,6 +45,7 @@ echo "misc times (TODO: merge w/ csv file)" > $LOG_FILE.times.log 2>&1
 
 echo
 rvm use ruby-2.3.1
+ruby --version
 for VARIABLE in 1 2 3 4 5
 do
   ruby ruby/bench.rb $QTY >> $LOG_FILE.csv
@@ -49,6 +54,7 @@ time ruby ruby/bench.rb $QTY >> $LOG_FILE.times.log 2>&1
 
 echo
 rvm use jruby-1.7.25
+ruby --version
 for VARIABLE in 1 2 3 4 5
 do
 jruby jruby/bench.rb $QTY >> $LOG_FILE.csv
@@ -57,6 +63,7 @@ time jruby jruby/bench.rb $QTY >> $LOG_FILE.times.log 2>&1
 
 echo
 rvm use mruby
+ruby --version
 for VARIABLE in 1 2 3 4 5
 do
 mruby mruby/bench.rb $QTY >> $LOG_FILE.csv
@@ -67,11 +74,14 @@ echo
 echo "Crystal 0.18.7 (non-release)"
 
 crenv local 0.18.7
+crystal version
 for VARIABLE in 1 2 3 4 5
 do
 crystal ./crystal/bench.cr $QTY >> $LOG_FILE.csv
 done
 time crystal ./crystal/bench.cr $QTY >> $LOG_FILE.times.log 2>&1
+
+## Stats for latest/production versions/mode:
 
 echo
 LOG_FILE='stats_latest'
@@ -84,6 +94,7 @@ echo "misc times (TODO: merge w/ csv file)" > $LOG_FILE.times.log 2>&1
 
 echo
 rvm use ruby-head
+ruby --version
 for VARIABLE in 1 2 3 4 5
 do
 ruby ruby/bench.rb $QTY >> $LOG_FILE.csv
@@ -92,6 +103,7 @@ time ruby ruby/bench.rb $QTY >> $LOG_FILE.times.log 2>&1
 
 echo
 rvm use jruby-9.1.2.0
+ruby --version
 # rvm use jruby-head
 for VARIABLE in 1 2 3 4 5
 do
@@ -101,6 +113,7 @@ time jruby jruby/bench.rb $QTY >> $LOG_FILE.times.log 2>&1
 
 echo
 rvm use mruby-head
+ruby --version
 ~/.rvm/rubies/mruby-head/bin/mrbc mruby/bench.rb
 for VARIABLE in 1 2 3 4 5
 do
@@ -116,6 +129,7 @@ echo
 echo "Crystal 0.18.7 (release)"
 
 crenv shell 0.18.7
+crystal version
 crystal build ./crystal/bench.cr --release
 mv bench ./crystal/bench
 for VARIABLE in 1 2 3 4 5
