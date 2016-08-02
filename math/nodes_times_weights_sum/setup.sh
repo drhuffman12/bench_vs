a5bf760d2c04de3232d2fb3a3046c1a7f7514d1f
Daniel Huffman <drhuffman12@yahoo.com>
2016-08-01 23:10:28 -0400

    Make RVM and CRENV happy

--- math/nodes_times_weights_sum/setup.sh
+++ math/nodes_times_weights_sum/setup.sh
@@ -1,5 +1,28 @@
 # bash setup.sh
 
+## Make RVM and CRENV happy:
+
+# exec $SHELL -l
+PATH="$HOME/.crenv/bin:$PATH"
+eval "$(crenv init -)"
+
+# Load RVM into a shell session *as a function*
+if [[ -s "$HOME/.rvm/scripts/rvm" ]] ; then
+
+  # First try to load from a user install
+  source "$HOME/.rvm/scripts/rvm"
+
+elif [[ -s "/usr/local/rvm/scripts/rvm" ]] ; then
+
+  # Then try to load from a root install
+  source "/usr/local/rvm/scripts/rvm"
+
+else
+
+  printf "ERROR: An RVM installation was not found.\n"
+
+fi
+
 rvm get stable --auto-dotfiles
 rvm get head
 rvm cleanup all
