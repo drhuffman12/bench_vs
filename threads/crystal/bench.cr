e5d6124cbfc987dd37e0a1e31eec064477e4e1c0
Daniel Huffman <drhuffman12@gmail.com>
2019-09-05 21:28:10 -0400

    bench for crystal

---
+++ threads/crystal/bench.cr
@@ -0,0 +1,35 @@
+
+require "benchmark"
+
+
+# # NOTE: The passed in `block` is expected to take an int arg
+  def bench_normal_vs_threaded(qty, &block : Int32 -> Array(Int32))
+  Benchmark.bm do |x|
+    x.report("normal") do
+      qty.times do |i| 
+        block.call(i)
+      end
+    end
+
+    # x.report("threads") do
+    #   threads = [] of Thread
+    #   qty.times do |i|
+    #     threads << Thread.new do     
+    #       block.call(i)
+    #     end
+    #   end
+    #   threads.map(&:join)
+    # end
+
+    x.report("fibers") do
+      qty.times do |i|
+        spawn do
+          block.call(i)
+        end
+      end
+      Fiber.yield
+    end
+  end
+end
+
+bench_normal_vs_threaded(10000) { |i| (0..i).to_a }
